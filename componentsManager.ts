6144f0e6ad42a464d6e92b3daa79cba723d335c7
TomC333 <d.dadiani343@gmail.com>
2024-06-25 15:04:05 +0400

    added forgotten export

---
+++ componentsManager.ts
@@ -0,0 +1,12 @@
+import { Profile } from "../models/profile";
+
+export class ComponentsManager{
+    
+    constructor() {
+
+    }
+
+    setProfiles(profiles: Profile[]): void{
+        // TO:DO
+    }
+}
